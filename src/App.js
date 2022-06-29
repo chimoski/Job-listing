5cf519ed08961ec50e36c5b6df2cad46d7120fa2
chimoski <chimaobiprince867@gmail.com>
2022-06-29 19:37:28 +0100

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { JobListItem } from './components/JobListItem';
+import React,{useState,useEffect } from 'react'
+import desktopImage from './images/bg-header-desktop.svg'
+import mobileImage from './images/bg-header-mobile.svg'
+import data from './data.json'
 function App() {
+  const [filters,  setFilters] = useState([]);
+  const [displayFilterBtns, setDisplayFilterBtns] = useState(true);
+  const [items, setItems] = useState([]);
+  useEffect(()=>{
+    setItems(data)
+  },[]);
+
+  const filterByStack = ({role,level,languages,tools })=>{
+    if(filters.length ===0){
+      return true
+    }
+    const stacks= [role,level];
+    if(languages) {
+      stacks.push(...languages);
+    }
+    if(tools) {
+      stacks.push(...tools);
+    }
+    return stacks.some(item=>filters.includes(item))
+  
+  }
+
+  const handleStackClick =(item)=>{
+    if(filters.includes(item)) return
+    setDisplayFilterBtns(true);
+    setFilters([...filters, item]);
+  }
+
+  const filteredItems = items.filter(filterByStack);
+  console.log(items);
+
+  const handleFilterClick =(filter)=>{
+    setFilters(filters.filter((f)=> f !==filter))
+  }
+ 
+  // const [filterBtns, setFilterBtns] = useState([])
+  // const {languages} = items
+
+
+  const handleClear = (e)=>{
+    e.preventDefault();
+    setDisplayFilterBtns(false);
+    setFilters([]);
+  }
+  // const handleFilterOnClick = (e)=>{
+  //   e.preventDefault()
+  //   // setFilterBtns([...languages]);
+  //   setDisplayFilterBtns(true);
+  // }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="">
+       <img
+       width='100%'
+       height="10vw"
+       className='hidden md:block'
+        src={desktopImage} alt="" />
+
+        <img
+       width='100%'
+       
+       className='md:hidden h-[30vw]'
+        src={mobileImage} alt="" />
       </header>
+
+        
+
+     <div className={`bg-[#effafa] ${displayFilterBtns  && filters.length>0 ? '':'pt-12'}`}>
+      {
+        displayFilterBtns && filters.length>0 && (
+          <div  className=' flex-wrap  
+        w-[80%] bg-[#fff] shadow-lg flex justify-between 
+         lg:items-center mx-auto p-4 translate-y-[-30px] rounded'>
+         <div className='flex gap-2 w-[80%] mr-3 flex-wrap'>
+          {
+            filters.length>0 && filters.map((filter,i)=>(
+         <div 
+         key={i}
+         className='flex gap-2 bg-[#eef6f6] font-bold items-center rounded'>
+          <p className='text-[#5ba4a4] text-sm  px-2'>{filter}</p>
+          <p
+          onClick={()=>handleFilterClick(filter)}
+           className='text-white bg-[#5ba4a4] text-bold px-2 cursor-pointer rounded-r hover:bg-black'>×</p>
+          </div>
+            ))
+          }
+         </div>
+          <div
+           onClick={handleClear}
+           className='text-gray-400 font-bold cursor-pointer hover:text-[#5ba4a4] hover:underline'
+          >
+            Clear
+          </div>
+      </div>
+        )
+      }
+    
+
+      {
+       filteredItems.map(item=>(
+         <JobListItem  
+          key={item.id} item={item}
+          handleStackClick={handleStackClick}
+          />
+        )) 
+      }
+    {/* <JobListItem /> */}
+     </div>
     </div>
   );
 }
---
+++ src/components/JobListItem.js
@@ -0,0 +1,88 @@
+
+// import img from '../images/photosnap.svg'
+
+export const JobListItem = ({item,handleStackClick}) => {
+
+ 
+  const { 
+    company,
+    logo,
+    featured,
+    position,
+    role,
+    level,
+    postedAt,
+    contract,
+    location,
+    languages,
+    tools} = item;
+
+    // const stacks = [...languages && languages, ...tools && tools,role,level]
+
+
+    const stacks= [role,level];
+
+    if(languages) {
+      stacks.push(...languages);
+    }
+    if(tools) {
+      stacks.push(...tools)
+    }
+    
+  return (
+    <main>
+    <div className="w-[80%] bg-[#fff]  lg:items-center
+    rounded-md px-5 pb-3 lg:flex lg:py-4 lg:gap-2 lg:justify-between
+    shadow-2xl mx-auto border-l-[#5ba4a4] border-l-8 lg:mb-5 mb-12">
+      {/* avatar */}
+     <div className=' lg:flex lg:gap-4 lg:mr-5rem lg:items-center'>
+     <img
+      className='rounded-full h-14 w-14 translate-y-[-25px] lg:translate-y-[0]'
+      src={logo} alt="logos" />
+      {/* description */}
+      <div className='flex flex-col gap-3'>
+       <div className='flex gap-x-2 items-center'>
+       <p className='pr-5 text-[#5ba4a4] font-bold'>{company}</p>
+        {item.new && 
+         <p className='text-white bg-[#5ba4a4] text-[12px] rounded-full sm:text-sm font-bold px-2 py-1'>
+         NEW!
+       </p>
+        }
+       
+        {featured && 
+        <h2 className='text-white text-[12px] sm:text-sm bg-black font-bold rounded-full px-2 py-1'>
+        FEATURED
+       </h2>
+        }
+        
+       </div>
+       <p className='font-bold hover:text-[#5ba4a4] cursor-pointer'>{position}</p>
+       <div className='flex gap-8 text-gray-500 font-semibold'>
+        <p className='text-[12px] sm:text-base'>{postedAt}</p>
+        <ul className='flex gap-8 list-disc text-[12px] sm:text-base'>
+          <li>{contract}</li>
+          <li>{location}</li>
+        </ul>
+       </div>
+      </div>
+     </div>
+      <div className='border-t w-full my-3 lg:hidden'></div>
+      {/* courses */}
+      <div className='flex gap-x-2 gap-y-2 flex-wrap'>
+        {
+          stacks ? (
+            stacks.map((stack,i)=>(
+              <button 
+              onClick={()=>handleStackClick(stack)}
+                key={i}
+               className='courses'>
+                {stack}
+                </button>
+            ))
+          ) :''
+        }
+      </div>
+    </div>
+  </main>
+  )
+}
